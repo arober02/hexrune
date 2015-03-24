d0484163a2317d074e981c981fd7d45bf59b163d
root <root@localhost>
2015-03-24 07:56:07 +0100

    vector operations added

--- hexrune.cpp
+++ hexrune.cpp
@@ -14,22 +14,57 @@ std::string to_string(T value)
 }
 
 // 2-dimensional vector class
-class CVector2D {
+class Vector2D {
   private:
-  float x=0;
-  float y=0;
+  float v[2];
   
   public:
+  Vector2D() {
+    v[0]=0;
+    v[1]=0;
+  }
+  
+  Vector2D(float vector[]) {
+    v[0]=vector[0];
+    v[1]=vector[1];
+  }
+  
+  
   float getX() {
-    return x;
+    return v[0];
+  }
+  
+  void setX(float value) {
+    v[0]=value;
   }
   
   float getY() {
-    return y;
+    return v[1];
+  }
+
+  void setY(float value) {
+    v[1]=value;
+  }
+  
+  float* getCoordinates() {
+    return v;
+  }
+  
+  void setCoordinates(float vector[]) {
+    v[0] = vector[0];
+    v[1] = vector[1];
+  }
+  
+  Vector2D operator+(Vector2D& vector) {
+    Vector2D vectorResult(vector.getCoordinates());
+    
+    vectorResult.setX(vectorResult.getX() + this->v[0]);
+    vectorResult.setY(vectorResult.getY() + this->v[1]);
+    return vectorResult;
   }
   
   float getLength() {
-    return sqrt(x*x + y*y);
+    return sqrt(getX()*getX() + getY()*getY());
   };
   
   string tostring() {
@@ -38,12 +73,12 @@ class CVector2D {
 };
 
 // Horizontally sitting regular hexagon shape class.
-class CHexagonH {
+class HexagonH {
   private:
-  CVector2D center;
+  Vector2D center;
   
   public:
-  CVector2D getCenter() {
+  Vector2D getCenter() {
   return center;
   };
   
@@ -62,7 +97,11 @@ class CHexagonH {
 
 
 int main(int argc,char* args[]) {
-  CHexagonH myHex;
+  HexagonH myHex;
+  float v1[2];
+  v1[0]=1;
+  v1[1]=2;
+  Vector2D myVector1(v1), myVector2;
   
   cout << "Hexrune kickoff" <<endl<< "==============="<<endl;
   cout << myHex.tostring();  
